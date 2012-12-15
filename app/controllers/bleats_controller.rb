25e39463ee09136940fe15674ea5c88e897709bc
John Brunton <john_brunton@hotmail.co.uk>
2012-12-15 15:20:23 -0500

    added create action to bleats controller

--- app/controllers/bleats_controller.rb
+++ app/controllers/bleats_controller.rb
@@ -1,2 +1,7 @@
 class BleatsController < ApplicationController
+  def create
+    @user = User.find(params[:user_id])
+    @bleat = @user.bleats.create(params[:bleat])
+    redirect_to user_path(@user)
+  end
 end
