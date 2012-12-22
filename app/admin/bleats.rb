801162ff7d37a51697c6a44a6f05a49c3b527e97
John Brunton <john_brunton@hotmail.co.uk>
2012-12-22 19:38:41 +0000

    customized admin interfaces

--- app/admin/bleats.rb
+++ app/admin/bleats.rb
@@ -1,3 +1,34 @@
 ActiveAdmin.register Bleat do
   controller.authorize_resource
+  
+  filter :content
+  filter :created_at
+  filter :user_email, :as => :string
+  filter :user_screen_name, :as => :string
+    
+  index do
+    selectable_column
+
+    column :user
+    column :content
+    column :created_at
+    
+    default_actions
+  end
+  
+  show do |ad|
+    attributes_table do
+      row :user
+      row :content
+      row :created_at
+    end
+    active_admin_comments
+  end
+  
+  form do |f|
+    f.inputs "Details" do
+      f.input :content
+    end
+    f.buttons
+  end
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,38 @@
 ActiveAdmin.register User do
-  controller.authorize_resource  
+  controller.authorize_resource
+  
+  filter :email
+  filter :screen_name
+  filter :created_at
+    
+  index do
+    selectable_column
+
+    column :email
+    column :screen_name
+    column :created_at
+    column :last_sign_in_at
+    
+    default_actions
+  end
+  
+  show do |ad|
+    attributes_table do
+      row :email
+      row :screen_name
+      row :created_at
+      row :last_sign_in_at
+      row :bio
+    end
+    active_admin_comments
+  end
+  
+  form do |f|
+    f.inputs "Details" do
+      f.input :email
+      f.input :screen_name
+      f.input :bio
+    end
+    f.buttons
+  end
 end
