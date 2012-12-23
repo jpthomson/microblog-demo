d92e9c61b1483fa328f32f2af7ff2e34fa230955
jpthomson <jthomson.home@gmail.com>
2012-12-23 19:28:06 +0000

    #7 - Added unique constraint to screen_name in users table

--- db/migrate/20121215152916_create_users.rb
+++ db/migrate/20121215152916_create_users.rb
@@ -9,5 +9,6 @@ class CreateUsers < ActiveRecord::Migration
       t.timestamps
     end
     add_index :users, :email, :unique => true
+    add_index :users, :screen_name, :unique => true
   end
 end
