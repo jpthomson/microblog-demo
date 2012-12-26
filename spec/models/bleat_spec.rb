9565c490b0b73e87465420bb32485d6343373fed
John Brunton <john_brunton@hotmail.co.uk>
2012-12-26 22:45:44 +0000

    changed bleat actions to appear depending on ownership, and added complete caching of partial

--- spec/models/bleat_spec.rb
+++ spec/models/bleat_spec.rb
@@ -7,4 +7,22 @@ describe Bleat do
       expect(bleat).to be_invalid
     end
   end
+  
+  describe "#owned_by?" do
+    it "returns true when passed the owner of the bleat" do
+      bleat = create(:bleat)
+      expect(bleat.owned_by?(bleat.user)).to be(true)
+    end
+    
+    it "returns false when passed another user" do
+      bleat = create(:bleat)
+      user = create(:user)
+      expect(bleat.owned_by?(user)).to be(false)
+    end
+    
+    it "returns false when passed nil" do
+      bleat = create(:bleat)
+      expect(bleat.owned_by?(nil)).to be(false)
+    end
+  end
 end
