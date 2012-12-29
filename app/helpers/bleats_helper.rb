6b80e3bb46b0b121f3c08accc6cd6dc5083e006d
jbrunton <john_brunton@hotmail.co.uk>
2012-12-29 22:50:19 +0000

    fixed bleat partial to link to hashtags correctly

--- app/helpers/bleats_helper.rb
+++ app/helpers/bleats_helper.rb
@@ -1,6 +1,6 @@
 module BleatsHelper
   def linkify_tag(tag)
-    link_to tag, controller: 'search', action: 'trending', tag: tag
+    link_to tag, controller: 'search', action: 'index', q: tag
   end
 
   def linkify_content(content)
--- spec/helpers/bleats_helper_spec.rb
+++ spec/helpers/bleats_helper_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe BleatsHelper do
   describe "#linkify_tag" do
     it "returns a link for the given tag" do
-      linky = link_to "#winning", controller: 'search', action: 'trending', tag: "#winning"
+      linky = link_to "#winning", controller: 'search', action: 'index', q: "#winning"
       expect(helper.linkify_tag "#winning").to eq(linky)
     end
   end
   
   describe "#linkify_content" do
     it "returns the content of a bleat with the hashtags linkified" do
-      linky = link_to "#winning", controller: 'search', action: 'trending', tag: "#winning"
+      linky = link_to "#winning", controller: 'search', action: 'index', q: "#winning"
       expect(helper.linkify_content "lol #winning").to eq("lol #{linky}")
     end
   end
